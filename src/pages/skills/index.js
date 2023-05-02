a10fefc2e63247d64c1165b929c656c700344a25
Shandea <86807815+Shandea@users.noreply.github.com>
2023-05-02 14:53:20 -0700

    initial setup

--- src/pages/skills/index.js
+++ src/pages/skills/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-     <h1>Hi</h1>
+     <h1>Skills</h1>
     </main>
   )
 }
